cb243aab9dafedef61323be3e4904d40ee894ed1
Ricardo Alves <ras@ic.ufal.br>
2022-01-10 01:53:45 -0300

    feat: pokemon types

---
+++ src/utils/pokemonTypeIcons.js
@@ -0,0 +1,41 @@
+import bug from 'assets/images/bug.png'
+import dark from 'assets/images/dark.png'
+import dragon from 'assets/images/dragon.png'
+import electric from 'assets/images/electric.png'
+import fairy from 'assets/images/fairy.png'
+import fighting from 'assets/images/fighting.png'
+import fire from 'assets/images/fire.png'
+import flying from 'assets/images/flying.png'
+import ghost from 'assets/images/ghost.png'
+import grass from 'assets/images/grass.png'
+import ground from 'assets/images/ground.png'
+import ice from 'assets/images/ice.png'
+import normal from 'assets/images/normal.png'
+import poison from 'assets/images/poison.png'
+import psychic from 'assets/images/psychic.png'
+import rock from 'assets/images/rock.png'
+import steel from 'assets/images/steel.png'
+import water from 'assets/images/water.png'
+
+const pokemonType = {
+  bug,
+  dark,
+  dragon,
+  electric,
+  fairy,
+  fighting,
+  fire,
+  flying,
+  ghost,
+  grass,
+  ground,
+  ice,
+  normal,
+  poison,
+  psychic,
+  rock,
+  steel,
+  water
+}
+
+export default pokemonType
